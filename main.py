a6fd3273f911de29cef395e430f760b9e2c2056e
shakhard <david.shakhar@techlib.cz>
2021-11-03 16:43:54 +0100

    reworked code to use functions

--- main.py
+++ main.py
@@ -1,34 +1,45 @@
 from art import logo
 import random
 
-number = random.randint(1, 100)
+EASY_LEVEL_TURNS = 10
+HARD_LEVEL_TURNS = 5
 
-print(logo)
-print("Welcome to the Number Guessing Game! ")
-print("I'm thinking of a number between 1 and 100.")
 
-difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
-
-if difficulty  == "easy":
-    attempts = 10
-else:
-    attempts = 5
-
-game_ended = False
-while not game_ended:
-    print(f"You have {attempts} attempts remaining to guess the number. Make them count.")
-    guess = int(input("Make a guess: "))
-
+def check_answer(guess, number, turns):
     if guess < number:
         print("Too low.")
+        return turns - 1
     elif guess > number:
         print("Too high.")
+        return turns - 1
     else:
         print(f"You got it! The answer was {number}")
-        game_ended = True
-    
-    attempts -= 1
-    
-    if attempts == 0:
-        game_ended = True
-        print("You've run out of guesses. LOSER!!!")
+
+
+def set_difficulty():
+    difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ").lower()
+    if difficulty == "easy":
+        return EASY_LEVEL_TURNS
+    else:
+        return HARD_LEVEL_TURNS
+
+
+def game():
+    print(logo)
+    print("Welcome to the Number Guessing Game! ")
+    print("I'm thinking of a number between 1 and 100.")
+    number = random.randint(1, 100)
+    turns = set_difficulty()
+    guess = 0
+
+    while guess != number:
+        print(
+            f"You have {turns} attempts remaining to guess the number. Make them count.")
+        guess = int(input("Make a guess: "))
+        turns = check_answer(guess, number, turns)  # VRACI CISLO!!!!!
+        if turns == 0:
+            print("You've run out of guesses. LOSER!!!")
+            return
+
+
+game()
